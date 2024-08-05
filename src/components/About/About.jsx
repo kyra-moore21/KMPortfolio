396820d329da54cec167dd2cd341234b2e710ba9
kyra-moore21 <kyra.moore21@icloud.com>
2024-08-05 10:45:02 -0400

    color scheme updates, icon attributes, new icons for about section, spacing edits in contact section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,19 +11,19 @@ const About = () => {
         <img src={getImageUrl("kyra2.png")} alt="Smiling photo of me
         " className={styles.aboutImage} />
         <ul className={styles.aboutItems}>
-            <li className={styles.aboutItem}><img src={getImageUrl("serverIcon.png")} alt="server icon"/>
+            <li className={styles.aboutItem}><img src={getImageUrl("fullstackIcon.png")} alt="server icon"/>
             <div  className={styles.aboutItemText}>
             <h3>Full Stack Developer</h3>
                 <p>I am skilled in both front and backend development</p>
             </div>
             </li>
-            <li className={styles.aboutItem}><img src={getImageUrl("uiIcon.png")} alt="UI Icon" />
+            <li className={styles.aboutItem}><img src={getImageUrl("ux.png")} alt="UI Icon" />
             <div className={styles.aboutItemText}>
             <h3>UX/UI Designer</h3>
             <p>I have a passion for building user friendly and acessible websites</p>
             </div>
             </li>
-            <li className={styles.aboutItem}><img src={getImageUrl("cursorIcon.png")} alt="Cursor Icon" />
+            <li className={styles.aboutItem}><img src={getImageUrl("paint-brush.png")} alt="Cursor Icon" />
             <div  className={styles.aboutItemText}>             
             <h3>Outside of coding</h3>
             <p>I love creating makeup looks, weightlifting, and exploring nature.</p>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,9 @@ import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <section className={styles.container}>
+    <section id='contact' >
+        
+      <div className={styles.container} >
         <div className={styles.Text}>
       <h1>Contact</h1>
       <h3>Connect with me!</h3>
@@ -17,7 +19,15 @@ const Contact = () => {
          <li className={styles.Link}><img src=
         {getImageUrl("githubIcon.png")} /><a href="https://github.com/kyra-moore21" >https://github.com/kyra-moore21</a></li>
       </ul>
+      </div>
+      <div className={styles.attributes}>
+        <p><a href="https://www.freepik.com/search">Icon by Freepik</a></p>
+        <p><a href="https://www.freepik.com/search">Icon by Pixel perfect</a></p>
+        <p><a href="https://www.freepik.com/search">Icon by Fajrul Fitrianto</a></p>
+        <p></p>
+      </div>
     </section>
+    
   )
 }
 
