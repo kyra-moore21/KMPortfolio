8b7b8a7e28fe5728a62fdc930f9a8ec77a5a1ee6
kyra-moore21 <kyra.moore21@icloud.com>
2024-08-23 10:56:03 -0400

    added tech connect project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import projects from "../../data/projects.json";
 import styles from "./Projects.module.css";
 import ProjectCard from './ProjectCard';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper-bundle.css';
+// import SwiperCore, { Navigation, Pagination } from 'swiper';
+
+//SwiperCore.use([Navigation, Pagination]);
 
 const Projects = () => {
-  return (
-    <section className={styles.container} id="projects">
-        <h2 className={styles.title}>Projects</h2>
-        <div className={styles.projects}>
-            {
-                projects.map((p, id) => {
-                    return (
-                        <ProjectCard key={id} p = {p}/>
-                    )
-                })
-            }
-        </div>
-    </section>
-  )
-}
+    return (
+      <section className={styles.container} id="projects">
+          <h2 className={styles.title}>Projects</h2>
+          <div className={styles.projects}>
+              {
+                  projects.map((p, id) => {
+                      return (
+                          <ProjectCard key={id} p = {p}/>
+                      )
+                  })
+              }
+          </div>
+      </section>
+    )
+  }
+  
 
 export default Projects
