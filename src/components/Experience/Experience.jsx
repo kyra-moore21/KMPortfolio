d3c47a2caa7db713038a11034cf1e7af7590c268
kyra-moore21 <kyra.moore21@icloud.com>
2025-06-10 12:55:14 -0400

    update experience header

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -7,7 +7,7 @@ import styles from "./Experience.module.css";
 const Skills = () => {
   return (
     <section  className={styles.container} id="experience">
-      <h2 className={styles.title} >Skills & Education</h2>
+      <h2 className={styles.title} >Skills & Experience</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
           {skills.map((skill, id) => {
